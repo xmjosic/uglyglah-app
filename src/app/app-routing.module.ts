45231de40166920b1bbd974b9e1161099908d726
xmjosic <xmjosic@gmail.com>
2021-02-23 00:07:20 +0100

    feat: login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'sign-up', component: SignupComponent }];
+const routes: Routes = [
+  { path: 'sign-up', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,13 +1,20 @@
+import { LoginResponse } from './../login/login.response.payload';
+import { LoginRequestPayload } from './../login/login.request.payload';
 import { SignupRequestPayload } from './../signup/signup-request.payload';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { LocalStorageService } from 'ngx-webstorage';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private localStorage: LocalStorageService
+  ) {}
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
     return this.httpClient.post(
@@ -15,5 +22,26 @@ export class AuthService {
       signupRequestPayload,
       { responseType: 'text' }
     );
+  }
+
+  login(loginRequestPayload: LoginRequestPayload): Observable<boolean> {
+    return this.httpClient
+      .post<LoginResponse>(
+        'http://localhost/api/v1/auth/login',
+        loginRequestPayload
+      )
+      .pipe(
+        map((data) => {
+          this.localStorage.store(
+            'authenticationToken',
+            data.authenticationToken
+          );
+          this.localStorage.store('username', data.username);
+          this.localStorage.store('refreshToken', data.refreshToken);
+          this.localStorage.store('expiresAt', data.expiresAt);
+
+          return true;
+        })
+      );
   }
 }
